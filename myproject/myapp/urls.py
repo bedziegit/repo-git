fd1690bc857a81c0b31337dbf91712750130457d
Your Name <you@example.com>
2024-12-20 13:32:24 +0100

    lab8

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("welcome", views.welcome_view),
     path("persons", views.person_list),
+    path('api/', include('myapp.api_urls'),)
 ]
